27477fb3b9f172bbbe6c8a9bc7d29d21b5c2e2af
AnaHerr <ana.herrera.91@gmail.com>
2022-02-09 17:11:07 +0100

    1st commit lab

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-require('dotenv').config();
+require('dotenv/config');
 
 const express = require('express');
+const res = require('express/lib/response');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -12,7 +14,40 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
-// Our routes go here:
+    // Our routes go here:
+
+app.get("/home", (req, res) => {
+  res.render("home");
+})
+
+/*
+app.get("/artist-search", (req, res) =>{
+    res.render("search-form")
+  })
+
+
+
+
+  app.get("/search-results", (req, res) =>{
+    const searchString = req.query.searchString;
+    const searchDate = req.query.searchDate;
+    
+    const searchResults = ["beatles", "nirvana"].filter((el)=> 
+    el.includes(searchString));
+    
+    res.render("search-results", {searchString, searchDate, searchResults})
+  });
+*/
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
